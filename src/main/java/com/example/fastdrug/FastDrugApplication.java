493a7d6768182fbefaa3e80ce0cd678a7e8af352
congsole <qax741@gmail.com>
2024-01-18 17:07:41 +0900

    first commit

---
+++ src/main/java/com/example/fastdrug/FastDrugApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastdrug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDrugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastDrugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastdrug/FastDrugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastdrug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastDrugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

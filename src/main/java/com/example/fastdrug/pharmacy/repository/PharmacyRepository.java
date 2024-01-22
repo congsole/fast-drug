e6f2a7c972fd39ceb5b2242facb7d167c448c7ae
congsole <qax741@gmail.com>
2024-01-22 11:09:01 +0900

    service test

---
+++ src/main/java/com/example/fastdrug/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,8 @@
+package com.example.fastdrug.pharmacy.repository;
+
+import com.example.fastdrug.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
---
+++ src/test/groovy/com/example/fastdrug/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -0,0 +1,66 @@
+package com.example.fastdrug.pharmacy.repository
+
+import com.example.fastdrug.AbstractIntegrationContainerBaseTest
+import com.example.fastdrug.pharmacy.entity.Pharmacy
+
+import org.springframework.beans.factory.annotation.Autowired
+
+
+
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
+    def "PharmacyRepository save"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+        .pharmacyAddress(address)
+        .pharmacyName(name)
+        .latitude(latitude)
+        .longitude(longitude)
+        .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLongitude() == longitude
+        result.getLatitude() == latitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+
+    }
+
+}

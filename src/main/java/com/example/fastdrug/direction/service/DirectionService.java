dd4b4b3497381fd6daeb3ebd15a4900d7ace7fc0
congsole <qax741@gmail.com>
2024-01-24 08:08:56 +0900

    길찾기 shorten URL 제공

--- src/main/java/com/example/fastdrug/direction/service/DirectionService.java
+++ src/main/java/com/example/fastdrug/direction/service/DirectionService.java
@@ -26,12 +26,19 @@ public class DirectionService {
     private final DirectionRepository directionRepository;
     private final PharmacySearchService pharmacySearchService;
     private final KakaoCategorySearchService kakaoCategorySearchService;
+    private final Base62Service base62Service;
 
     @Transactional
     public List<Direction> saveAll(List<Direction> directionList) {
         if(CollectionUtils.isEmpty(directionList)) return Collections.emptyList();
         return directionRepository.saveAll(directionList);
     }
+
+    public Direction findById(String encodedId) {
+        Long decodedId = base62Service.decodeDirectionId(encodedId);
+        return directionRepository.findById(decodedId).orElse(null);
+    }
+
     public List<Direction> buildDirectionList(DocumentDto dto) {
         if(Objects.isNull(dto)) {
             return Collections.emptyList();
